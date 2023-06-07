c5b24e43ecf8f069ba5277698d80774762f69659
carlos gette <116603034+chars2711@users.noreply.github.com>
2023-06-07 11:44:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+const result = document.querySelector('.result');
+const form = document.querySelector('.preguntas');
+const nameCiu = document.querySelector('#ciudad');
+const namePais = document.querySelector('#pais');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (nameCiu.value === '' || namePais.value === '') {
+        ErrorFatal('Completa todos los espacios');
+        return;
+    }
+
+    callAPI(nameCiu.value, namePais.value);
+
+})
+
+function callAPI(ciu, pais){
+    const apiId = '8f47ba0d606dd0d6345d27910c44c38c';
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciu},${pais}&appid=${apiId}`;
+
+    fetch(url)
+        .then(data => {
+            return data.json();
+        })
+        .then(dataJSON => {
+            if (dataJSON.cod === '404') {
+                ErrorFatal('error');
+            } else {
+                clearHTML();
+                showWeather(dataJSON);
+            }
+
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+function showWeather(data){
+    const {name, main:{temp, temp_min, temp_max, humidity, feels_like, pressure}, weather:[arr]} = data;
+
+    const degrees = DatosListos(temp);
+    const min = DatosListos(temp_min);
+    const max = DatosListos(temp_max);
+    const humedad = DatosListos(humidity);
+    const sensacion = DatosListos(feels_like);
+    const presion = DatosListos(pressure);
+
+    const content = document.createElement('div');
+    content.innerHTML = `
+        <h2>Clima en ${name}</h2>
+        <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
+        <h2>${degrees}°C</h2>
+        <p>Max: ${max}°C</p>
+        <p>Min: ${min}°C</p>
+        <p>Humedad: ${humedad}</p>
+        <p>Sensación Termica: ${sensacion}°C</p>
+        <p>Presíon atmosférica: ${presion}</p>`;
+
+    result.appendChild(content);
+
+}
+
+function ErrorFatal(message){
+    const alert = document.createElement('p');
+    alert.classList.add('alert-message');
+    alert.innerHTML = message;
+
+    form.appendChild(alert);
+    setTimeout(() => {
+        alert.remove();
+    }, 3000);
+}
+
+function DatosListos(temp){
+    return parseInt(temp - 273.15);
+}
+
+function clearHTML(){
+    result.innerHTML = '';
+}
